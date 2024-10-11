bc022f70de3b79b0148bb5f2e7ab368b585fac45
lefarmer01600 <t.clemenceau@outlook.fr>
2024-10-11 15:12:20 +0200

    added anim for nav bar and change color on scroll

--- js/animation.js
+++ js/animation.js
@@ -3,8 +3,28 @@ window.addEventListener(
     () => {
         document.body.style.setProperty(
             "--scroll",
-            (window.scrollY / (document.body.offsetHeight - window.innerHeight))*-30
+            (window.scrollY / (document.body.offsetHeight - window.innerHeight)) * -30
         );
+        if ((window.scrollY / (document.body.offsetHeight - window.innerHeight)) * -30 < -0.1) {
+            document.getElementById("nav-btn-contact").innerHTML = "<a>Nous contacter</a>"
+            document.getElementById("nav-btn-contact").classList.add("join-item")
+            document.getElementById("nav-btn-contact").classList.add("btn")
+            document.getElementById("nav-group").classList.remove("rounded-r-full")
+            // "<li id='nav-btn-contact' class='btn join-item'><a>Nous contacter</a></li>"
+        } else {
+            document.getElementById("nav-btn-contact").innerHTML = ""
+            document.getElementById("nav-btn-contact").classList.remove("join-item")
+            document.getElementById("nav-btn-contact").classList.remove("btn")
+            document.getElementById("nav-group").classList.add("rounded-r-full")
+        }
+
+        if ((window.scrollY / (document.body.offsetHeight - window.innerHeight)) * -30 < -27) {
+            document.getElementById("nav-group").classList.add("bg-slate-400")
+            document.getElementById("nav-group").classList.remove("glass")
+        } else {
+            document.getElementById("nav-group").classList.remove("bg-slate-400")
+            document.getElementById("nav-group").classList.add("glass")
+        }
     },
     false
 );
