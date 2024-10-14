e422ead474776b85a4ca6d72aafa0dc556fad6be
Christian <christiancallof74@gmail.com>
2024-10-14 15:53:25 +0200

    results page

--- server.py
+++ server.py
@@ -30,15 +30,41 @@ def jeux():
 @app.route('/quiz', methods=['GET'])
 def quiz():
     global quiz_question
-    i = 0
-    #make i = to the get parameter
-    if request.args.get('page'):
-        i = request.args.get('page')
+    i = 0  # Par défaut, on affiche la première question
 
-    # return render_template('home.html', question=question)
+    # Si le paramètre 'page' est passé dans l'URL, on récupère la page actuelle
+    if request.args.get('page'):
+        i = int(request.args.get('page'))  # Convertit la chaîne en entier
+
+    # On renvoie la page avec la question correspondante
     return render_template('Quiz1.html', data=quiz_question["quiz"], i=i)
 
-@app.route('/page', methods=['POST'])
+@app.route('/results', methods=['GET', 'POST'])
+def results():
+    global quiz_question
+    score = 0
+    total_questions = len(quiz_question["quiz"])
+    
+    # On boucle sur toutes les questions et vérifie les réponses soumises
+    for i in range(total_questions):
+        user_answer = request.form.get(f'radio-{i}')  # Récupère la réponse soumise
+        correct_answer = quiz_question["quiz"][i]["correctAnswer"]  # Réponse correcte
+        if user_answer == correct_answer:
+            score += 1
+
+    # Message en fonction du score
+    if score < 5:
+        message = "Réessayez, ah c'est bof refait"
+    elif score >= 5 and score < 9:
+        message = "Bravo, vous avez bien compris"
+    else:
+        message = "Félicitations, vous avez tout compris !"
+    
+    # Rendre la page des résultats
+    return render_template('results.html', score=score, total_questions=total_questions, message=message)
+
+
+@app.route('/page', methods=['GET'])
 def submit():
     global quiz_question
     i = 1
